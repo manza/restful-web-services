a35512e214a5e85f78f71d04514a51b451201e3b
Reinaldo Manzano Junior <reinaldo.manzano@gmail.com>
2018-04-03 23:12:00 -0300

    i18n

--- src/main/java/com/manza/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/manza/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,6 +2,13 @@ package com.manza.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -9,4 +16,21 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+//        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+// Replaced by application.properties
+//    @Bean
+//    public ResourceBundleMessageSource messageSource() {
+//	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//	    messageSource.setBasename("messages");
+//	    return messageSource;
+//    }
+
 }
