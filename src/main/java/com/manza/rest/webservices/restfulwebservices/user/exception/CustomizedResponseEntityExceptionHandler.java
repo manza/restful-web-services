ae07aa0d2af565edb9516b8d95991a5e55087072
Reinaldo Manzano Junior <reinaldo.manzano@gmail.com>
2018-04-03 23:11:31 -0300

    validation error custom

--- src/main/java/com/manza/rest/webservices/restfulwebservices/user/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/manza/rest/webservices/restfulwebservices/user/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.manza.rest.webservices.restfulwebservices.user.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +40,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), "Validation failed", ex.getBindingResult().toString());
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
